19da6d3e2a85a3c6a71e4a1c1d28785ed921653a
ArthurFernandes06 <artrdgs@gmail.com>
2025-06-15 06:51:42 +0000

    Ajeitando o form

--- backend/gabarito_oci/settings.py
+++ backend/gabarito_oci/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'gabarito_oci',
     'home',
     'cadastro',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
