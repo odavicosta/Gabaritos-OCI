ab52da57e2bcb846273c276c22d3d1a356c883ad
ArthurFernandes06 <artrdgs@gmail.com>
2025-06-15 20:50:43 +0000

    corrigi a pagina home

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.contrib.auth.decorators import login_required
+from . leitor_interface import ler_prova_por_dados
+from django.http import JsonResponse
+import os
+
+
+
 
 @login_required
 def index(request):
-    
-    return render(request,'home/home.html')
+    if request.method == 'POST' and 'imagem' in request.FILES:
+        imagem = request.FILES['imagem']
+        dados_bytes = imagem.read()
+        extensao = os.path.splitext(imagem.name)[1]
+        resultado = ler_prova_por_dados(extensao, dados_bytes)
+        return JsonResponse(resultado)  
 
-# Create your views here.
+    return render(request, 'home/home.html')
+
+
